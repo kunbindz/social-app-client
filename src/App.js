fda88f337f331a4924784c055ad3609066fffd20
Loc tran <8598loctran@gmail.com>
2021-02-07 01:31:00 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Alert from "./components/alert/Alert";
+import PageRender from "./pageRender";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import { refreshToken } from "./redux/actions/authAction";
 function App() {
+  const { auth } = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshToken());
+  }, [dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Alert />
+      <input type="checkbox" id="theme" />
+      <div className="App">
+        <div className="main">
+          <Route exact path="/" component={auth.token ? Home : Login} />
+          <Route exact path="/:page" component={PageRender} />
+          <Route exact path="/:page/:id" component={PageRender} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
